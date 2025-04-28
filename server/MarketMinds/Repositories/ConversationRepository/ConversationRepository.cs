a392f4e72fee3d37bbf8002b3d1678e39635ba05
luca <lucac375@gmail.com>
2025-04-28 17:37:35 +0300

    move irepo to shared

--- server/MarketMinds/Repositories/ConversationRepository/ConversationRepository.cs
+++ server/MarketMinds/Repositories/ConversationRepository/ConversationRepository.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using MarketMinds.Shared.Models;
+using MarketMinds.Shared.IRepository;
+using MarketMinds.Shared.IRepository.ConversationRepository;
 using Server.DataAccessLayer;
 
 namespace MarketMinds.Repositories.ConversationRepository
