f78019c1ba3f1ececc76d8183670736583f78459
Nanu25 <alexandrugrancea25@gmail.com>
2025-05-12 18:08:50 +0300

    a

--- MarketMinds.Test/Services/BorrowProductsServiceTest/BorrowProductsRepositoryMock.cs
+++ MarketMinds.Test/Services/BorrowProductsServiceTest/BorrowProductsRepositoryMock.cs
@@ -1,101 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MarketMinds.Shared.Models;
-using MarketMinds.Repositories.BorrowProductsRepository;
-using MarketMinds.Repositories;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using MarketMinds.Shared.Models;
+//using MarketMinds.Repositories.BorrowProductsRepository;
+//using MarketMinds.Repositories;
 
-namespace MarketMinds.Test.Services.BorrowProductsServiceTest
-{
-    public class BorrowProductsRepositoryMock : IBorrowProductsRepository
-    {
-        private List<Product> products;
-        private int createListingCount;
-        private int deleteListingCount;
-        private int getProductsCount;
-        private int getProductByIdCount;
-        private int updateProductCount;
+//namespace MarketMinds.Test.Services.BorrowProductsServiceTest
+//{
+//    public class BorrowProductsRepositoryMock : IBorrowProductsRepository
+//    {
+//        private List<Product> products;
+//        private int createListingCount;
+//        private int deleteListingCount;
+//        private int getProductsCount;
+//        private int getProductByIdCount;
+//        private int updateProductCount;
 
-        public BorrowProductsRepositoryMock()
-        {
-            products = new List<Product>();
-            createListingCount = 0;
-            deleteListingCount = 0;
-            getProductsCount = 0;
-            getProductByIdCount = 0;
-            updateProductCount = 0;
-        }
+//        public BorrowProductsRepositoryMock()
+//        {
+//            products = new List<Product>();
+//            createListingCount = 0;
+//            deleteListingCount = 0;
+//            getProductsCount = 0;
+//            getProductByIdCount = 0;
+//            updateProductCount = 0;
+//        }
 
-        public void AddProduct(Product product)
-        {
-            if (!(product is BorrowProduct))
-            {
-                throw new InvalidCastException("Product must be of type BorrowProduct");
-            }
+//        public void AddProduct(Product product)
+//        {
+//            if (!(product is BorrowProduct))
+//            {
+//                throw new InvalidCastException("Product must be of type BorrowProduct");
+//            }
 
-            products.Add(product);
-            createListingCount++;
-        }
+//            products.Add(product);
+//            createListingCount++;
+//        }
 
-        public void DeleteProduct(Product product)
-        {
-            products.RemoveAll(p => p.Id == product.Id);
-            deleteListingCount++;
-        }
+//        public void DeleteProduct(Product product)
+//        {
+//            products.RemoveAll(p => p.Id == product.Id);
+//            deleteListingCount++;
+//        }
 
-        public List<Product> GetProducts()
-        {
-            getProductsCount++;
-            return products;
-        }
+//        public List<Product> GetProducts()
+//        {
+//            getProductsCount++;
+//            return products;
+//        }
 
-        public Product GetProductByID(int productId)
-        {
-            getProductByIdCount++;
-            return products.FirstOrDefault(p => p.Id == productId);
-        }
+//        public Product GetProductByID(int productId)
+//        {
+//            getProductByIdCount++;
+//            return products.FirstOrDefault(p => p.Id == productId);
+//        }
 
-        public void UpdateProduct(Product product)
-        {
-            if (!(product is BorrowProduct))
-            {
-                throw new InvalidCastException("Product must be of type BorrowProduct");
-            }
+//        public void UpdateProduct(Product product)
+//        {
+//            if (!(product is BorrowProduct))
+//            {
+//                throw new InvalidCastException("Product must be of type BorrowProduct");
+//            }
 
-            var existingProduct = products.FirstOrDefault(p => p.Id == product.Id);
-            if (existingProduct != null)
-            {
-                products.Remove(existingProduct);
-                products.Add(product);
-            }
-            updateProductCount++;
-        }
+//            var existingProduct = products.FirstOrDefault(p => p.Id == product.Id);
+//            if (existingProduct != null)
+//            {
+//                products.Remove(existingProduct);
+//                products.Add(product);
+//            }
+//            updateProductCount++;
+//        }
 
-        public int GetCreateListingCount()
-        {
-            return createListingCount;
-        }
+//        public int GetCreateListingCount()
+//        {
+//            return createListingCount;
+//        }
 
-        public int GetDeleteListingCount()
-        {
-            return deleteListingCount;
-        }
+//        public int GetDeleteListingCount()
+//        {
+//            return deleteListingCount;
+//        }
 
-        public int GetGetProductsCount()
-        {
-            return getProductsCount;
-        }
+//        public int GetGetProductsCount()
+//        {
+//            return getProductsCount;
+//        }
 
-        public int GetGetProductByIdCount()
-        {
-            return getProductByIdCount;
-        }
+//        public int GetGetProductByIdCount()
+//        {
+//            return getProductByIdCount;
+//        }
 
-        public int GetUpdateProductCount()
-        {
-            return updateProductCount;
-        }
-    }
-}
+//        public int GetUpdateProductCount()
+//        {
+//            return updateProductCount;
+//        }
+//    }
+//}
--- MarketMinds.Test/Services/BuyProductsServiceTest/BuyProductsRepositoryMock.cs
+++ MarketMinds.Test/Services/BuyProductsServiceTest/BuyProductsRepositoryMock.cs
@@ -1,99 +1,99 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using MarketMinds.Shared.Models;
-using MarketMinds.Repositories.BuyProductsRepository;
-using MarketMinds.Repositories;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using MarketMinds.Shared.Models;
+//using MarketMinds.Repositories.BuyProductsRepository;
+//using MarketMinds.Repositories;
 
-namespace MarketMinds.Test.Services.BuyProductsServiceTest
-{
-    public class BuyProductsRepositoryMock : IBuyProductsRepository
-    {
-        private List<Product> products;
-        private int createListingCount;
-        private int deleteListingCount;
-        private int getProductsCount;
-        private int getProductByIdCount;
-        private int updateProductCount;
+//namespace MarketMinds.Test.Services.BuyProductsServiceTest
+//{
+//    public class BuyProductsRepositoryMock : IBuyProductsRepository
+//    {
+//        private List<Product> products;
+//        private int createListingCount;
+//        private int deleteListingCount;
+//        private int getProductsCount;
+//        private int getProductByIdCount;
+//        private int updateProductCount;
 
-        public BuyProductsRepositoryMock()
-        {
-            products = new List<Product>();
-            createListingCount = 0;
-            deleteListingCount = 0;
-            getProductsCount = 0;
-            getProductByIdCount = 0;
-            updateProductCount = 0;
-        }
+//        public BuyProductsRepositoryMock()
+//        {
+//            products = new List<Product>();
+//            createListingCount = 0;
+//            deleteListingCount = 0;
+//            getProductsCount = 0;
+//            getProductByIdCount = 0;
+//            updateProductCount = 0;
+//        }
 
-        public void AddProduct(Product product)
-        {
-            if (!(product is BuyProduct))
-            {
-                throw new InvalidCastException("Product must be of type BuyProduct");
-            }
+//        public void AddProduct(Product product)
+//        {
+//            if (!(product is BuyProduct))
+//            {
+//                throw new InvalidCastException("Product must be of type BuyProduct");
+//            }
 
-            products.Add(product);
-            createListingCount++;
-        }
+//            products.Add(product);
+//            createListingCount++;
+//        }
 
-        public void DeleteProduct(Product product)
-        {
-            products.RemoveAll(p => p.Id == product.Id);
-            deleteListingCount++;
-        }
+//        public void DeleteProduct(Product product)
+//        {
+//            products.RemoveAll(p => p.Id == product.Id);
+//            deleteListingCount++;
+//        }
 
-        public List<Product> GetProducts()
-        {
-            getProductsCount++;
-            return products;
-        }
+//        public List<Product> GetProducts()
+//        {
+//            getProductsCount++;
+//            return products;
+//        }
 
-        public Product GetProductByID(int productId)
-        {
-            getProductByIdCount++;
-            return products.FirstOrDefault(p => p.Id == productId);
-        }
+//        public Product GetProductByID(int productId)
+//        {
+//            getProductByIdCount++;
+//            return products.FirstOrDefault(p => p.Id == productId);
+//        }
 
-        public void UpdateProduct(Product product)
-        {
-            if (!(product is BuyProduct))
-            {
-                throw new InvalidCastException("Product must be of type BuyProduct");
-            }
+//        public void UpdateProduct(Product product)
+//        {
+//            if (!(product is BuyProduct))
+//            {
+//                throw new InvalidCastException("Product must be of type BuyProduct");
+//            }
 
-            var existingProduct = products.FirstOrDefault(p => p.Id == product.Id);
-            if (existingProduct != null)
-            {
-                products.Remove(existingProduct);
-                products.Add(product);
-            }
-            updateProductCount++;
-        }
+//            var existingProduct = products.FirstOrDefault(p => p.Id == product.Id);
+//            if (existingProduct != null)
+//            {
+//                products.Remove(existingProduct);
+//                products.Add(product);
+//            }
+//            updateProductCount++;
+//        }
 
-        public int GetCreateListingCount()
-        {
-            return createListingCount;
-        }
+//        public int GetCreateListingCount()
+//        {
+//            return createListingCount;
+//        }
 
-        public int GetDeleteListingCount()
-        {
-            return deleteListingCount;
-        }
+//        public int GetDeleteListingCount()
+//        {
+//            return deleteListingCount;
+//        }
 
-        public int GetGetProductsCount()
-        {
-            return getProductsCount;
-        }
+//        public int GetGetProductsCount()
+//        {
+//            return getProductsCount;
+//        }
 
-        public int GetGetProductByIdCount()
-        {
-            return getProductByIdCount;
-        }
+//        public int GetGetProductByIdCount()
+//        {
+//            return getProductByIdCount;
+//        }
 
-        public int GetUpdateProductCount()
-        {
-            return updateProductCount;
-        }
-    }
-}
+//        public int GetUpdateProductCount()
+//        {
+//            return updateProductCount;
+//        }
+//    }
+//}
